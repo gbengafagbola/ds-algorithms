27e77b954217d6007ae057e8d72669444eba8bf3
holard <fagbolagbenga2015@gmail.com>
2022-03-28 10:56:34 +0100

    freq-counter solution with O(N) Time complexity

--- frequency-counter/qs-1.js
+++ frequency-counter/qs-1.js
@@ -27,6 +27,53 @@ function same(arr1, arr2) {
 }
 
 // test case
-//same([1,2,3,2,5],[4,9,4,1])
+//same([1,2,3,2,5],[4,9,4,1]) // true
 
 // Time Complexity : O(N**2)
+
+
+// Solution 2
+function same(arr1, arr2) {
+
+    if (arr1.length !== arr2.length){
+        return false;    
+    }
+    
+    let freqCounter1 = {};
+    let freqCounter2 = {};
+
+    for(let val of arr1){
+        freqCounter1[val] = (freqCounter1[val] || 0) +1      
+    }
+
+    for(let val of arr2){
+        freqCounter2[val] = (freqCounter2[val] || 0) +1      
+    }
+
+    
+    for(let key in freqCounter1){
+        
+        if(!(key ** 2 in freqCounter2)){
+            return false
+        }
+
+        if(freqCounter2[key ** 2] !== freqCounter1[key]) {
+            return false;
+        }
+    }
+    
+   return true;
+   
+}
+
+
+// test case
+//same([1,2,3,2,5],[4,9,4,1]) //false
+// same([1,2,3,2,5],[9,1,4,4,11]) //false
+
+// Time Complexity : O(N)
+
+
+
+
+
