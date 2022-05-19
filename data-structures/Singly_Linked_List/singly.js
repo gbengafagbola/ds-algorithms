bf2871a9c49ba3ebc96537418397d351fc90f120
holard <fagbolagbenga2015@gmail.com>
2022-05-19 15:55:55 +0100

    sigly:shift

--- data-structures/Singly_Linked_List/singly.js
+++ data-structures/Singly_Linked_List/singly.js
@@ -1,4 +1,3 @@
-
 class Node{
     constructor(val){
         this.val = val;
@@ -17,19 +16,20 @@ class SinglyLinkedList {
     
     push(val){
         var newNode = new Node(val); 
-        if(!this.head){                     // or if(this.head === null) {if there is no element yet then set the head and tail to the val }
+
+        if(!this.head){                    // or if(this.head === null) {if there is no element yet then set the head to the value and tail to the head } #edgecase
             this.head = newNode;
-            this.tail = newNode;
-        } else {                           // if there is an head then set the current value to the tail and the next-incoming value to the tail and the current 
-            this.tail = newNode;
-            this.tail.next = newNode;
+            this.tail = this.head;         // same as setting both head and tail initially to the newNode
+        } else {                           // if there is an head i.e the list is not empty then  
+            this.tail.next = newNode;      // next-incoming value to the tail and
+            this.tail = newNode;           // we update the tail to ensure it points to the newNode
         }
         this.length++;
         return this;
     }
 
     pop(){
-        if(!this.head) return undefined;
+        if(!this.head) return undefined;   //#edgecase
         
         var scout = this.head;
         var upcomingTail = scout;
@@ -42,15 +42,32 @@ class SinglyLinkedList {
             this.tail = upcomingTail;     // persist the tail
             this.tail.next = null         // inform that there is no connection with the just popped element by setting to null
             this.length--;                // update the reduction of the length due to the popped off item
-            if(this.length === 0){        // after reducing the length to 0, we have to set the head and tail to null
+            if(this.length === 0){        // after reducing the length to 0, we have to set the head and tail to null #edgecase
                 this.head = null;
-                this.tail = null
+                this.tail = null;
             }
             return scout;
+    }
+
+    shift(){
+        if(!this.head) return undefined;    //#edgecase
+
+        var currentHead = this.head;       // instantiate the current head we want to remove
+        this.head = currentHead.next;       // set the new head to the next item in the list
+        this.length--;                     // remeber to decrement the length
+        if(this.length === 0){             // if the length of the list is 0 then set tail to null #edgecase
+            this.tail = null;
         }
+        return currentHead;
+
+    }
+
 }
 
 
-
-
-// var list = new SinglyLinkedList()
+var list = new SinglyLinkedList()
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+list.push(5)
