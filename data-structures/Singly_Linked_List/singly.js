32bc18abfc6cc94bc72d8ee03e6b3476bd6c36cc
holard <fagbolagbenga2015@gmail.com>
2022-05-19 11:45:56 +0100

    singly linked list pop method

--- data-structures/Singly_Linked_List/singly.js
+++ data-structures/Singly_Linked_List/singly.js
@@ -6,6 +6,8 @@ class Node{
     }
 }
 
+
+
 class SinglyLinkedList {
     constructor(){
         this.head = null;
@@ -15,16 +17,37 @@ class SinglyLinkedList {
     
     push(val){
         var newNode = new Node(val); 
-        if(this.head === null){     // or if(!this.head) {if there is no element yet then set the head and tail to the val }
+        if(!this.head){                     // or if(this.head === null) {if there is no element yet then set the head and tail to the val }
             this.head = newNode;
             this.tail = newNode;
-        } else {                   // if there is an head then set the current value to the tail and the next-incoming value to the tail and the current 
+        } else {                           // if there is an head then set the current value to the tail and the next-incoming value to the tail and the current 
             this.tail = newNode;
             this.tail.next = newNode;
         }
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        
+        var scout = this.head;
+        var upcomingTail = scout;
+
+            while(scout.next){             // while there's still a next item or incoming item, 
+                upcomingTail = scout;      // upcoming now becomes scout &
+                scout = scout.next         // scout becomes the incoming next item
+            }
+
+            this.tail = upcomingTail;     // persist the tail
+            this.tail.next = null         // inform that there is no connection with the just popped element by setting to null
+            this.length--;                // update the reduction of the length due to the popped off item
+            if(this.length === 0){        // after reducing the length to 0, we have to set the head and tail to null
+                this.head = null;
+                this.tail = null
+            }
+            return scout;
+        }
 }
 
 
