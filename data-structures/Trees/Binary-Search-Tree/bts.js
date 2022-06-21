b4e66a06348c5a97f7dadd840c4c1cb0b0b21297
holard <fagbolagbenga2015@gmail.com>
2022-06-21 15:51:13 +0100

    tree/BTS:BFS-inOrder

--- data-structures/Trees/Binary-Search-Tree/bts.js
+++ data-structures/Trees/Binary-Search-Tree/bts.js
@@ -107,6 +107,19 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+
+    DFSPinOrder(){
+        var data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
 
 
