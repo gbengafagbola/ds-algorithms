f5e96d807e6a0eb5062ce6cc8e4959294b328d52
holard <fagbolagbenga2015@gmail.com>
2022-06-21 15:23:16 +0100

    tree/BTS:BFS-postOrder

--- data-structures/Trees/Binary-Search-Tree/bts.js
+++ data-structures/Trees/Binary-Search-Tree/bts.js
@@ -93,6 +93,20 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+
+    DFSPostOrder(){
+        var data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
 
 
