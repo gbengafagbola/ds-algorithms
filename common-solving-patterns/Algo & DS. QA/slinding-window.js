c2140797bf50fd23b493250e2055f8db0f9098d6
holard <fagbolagbenga2015@gmail.com>
2022-07-25 21:36:43 +0100

    slinding window

---
+++ common-solving-patterns/Algo & DS. QA/slinding-window.js
@@ -0,0 +1,22 @@
+// Given an integer array and a window of size w, find the current maximum value in the window as it slides through the entire array.
+
+
+let findMaxSlidingWindow = function(nums, windowSize) {
+    var result = [];
+
+    if(nums.length == 0) {
+        return result;
+      }
+      
+    //Write your code
+    let i = 0;
+    let j = windowSize;
+  
+      while(i <= nums.length - windowSize){   
+          const temp = nums.slice(i, j);
+          result.push(Math.max(...temp));
+          i++; j++;
+      }
+  
+      return result; 
+  };
